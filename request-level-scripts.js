6a4dd8f154ceeea1acdfdb3fe20eeb88263440f3
Bryan Cross <bryanrkcross@gmail.com>
2022-07-28 12:41:48 -0500

    Updating to show layered testing

--- request-level-scripts.js
+++ request-level-scripts.js
@@ -1,12 +1,12 @@
-  pm.test("Status code is 200", function () {
+  pm.test("REQUEST Status code is 200", function () {
       pm.response.to.have.status(200);
     });
-    pm.test("Response time is less than 500ms", function () {
+    pm.test("REQUEST Response time is less than 500ms", function () {
         pm.expect(pm.response.responseTime).to.be.below(500);
     });
-    pm.test("Content-Type is present", function () {
+    pm.test("REQUEST Content-Type is present", function () {
         pm.response.to.have.header("Content-Type");
     });
-        pm.test("Status code is 200", function () {
+        pm.test("REQUEST Status code is 200", function () {
       pm.response.to.have.status(200);
     });
